44ea7542391d43b6d1b6bd711abde3f323cb0212
Tom Rosenkranz <tom@socialcat.de>
2022-11-22 16:15:09 +0100

    favicon upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,13 @@
-<template>  
-    <div class="page-container">
-        <div class="outer-container">
-            <LeftNav />
-            <slot />
-    
-        </div> 
+<script setup>
+useHead({
+  link: [{ rel: "icon", type: "image/x-icon", href: "/img/favicon.png" }],
+});
+</script>
+<template>
+  <div class="page-container">
+    <div class="outer-container">
+      <LeftNav />
+      <slot />
     </div>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-
-   modules: ['@nuxtjs/prismic'],
-   prismic: { 
-      endpoint: 'portolio-feierabend',
-       clientConfig: {
-         routes: [
-             {
-                 type: 'werk',
-                 path: '/:uid'
-             }
-         ]
-     }
-
-
-},
-   css: ["~/assets/css/styles.css"]
-
-})
+  modules: ["@nuxtjs/prismic"],
+  prismic: {
+    endpoint: "portolio-feierabend",
+    clientConfig: {
+      routes: [
+        {
+          type: "werk",
+          path: "/:uid",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/styles.css"],
+});
